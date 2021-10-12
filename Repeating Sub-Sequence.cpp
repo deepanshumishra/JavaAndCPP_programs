d9f5f5333827104847c1567ce3d1e823f5d00cd1
Yahiko <52419760+nirajninja@users.noreply.github.com>
2021-10-12 17:40:26 +0530

    Create Repeating Sub-Sequence.cpp
    interview bit dp problem soln

---
+++ Repeating Sub-Sequence.cpp
@@ -0,0 +1,30 @@
+int Solution::anytwo(string A) {
+    
+    int n=A.size();
+    int dp[n+1][n+1];
+    for(int i=0;i<n+1;i++)
+    {
+        for(int j=0;j<n+1;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j]=0;
+            }
+            else if(A[i-1] == A[j-1] && i!=j)
+            {
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    
+    if(dp[n][n]>=2)
+    {
+        return 1;
+    }
+    
+    return 0;// dp[n][n];
+    
+}
